22d5c8d2a00be4a06c287a63a34a3efbc4ffb2c4
Kelvin <kelvenchendu@gmail.com>
2019-06-27 03:04:44 -0300

    Api added.

--- backend/ceeapp/admin.py
+++ backend/ceeapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Area, Company, Event
+
+admin.site.register(Area)
+admin.site.register(Company)
+admin.site.register(Event)
--- backend/project/settings.py
+++ backend/project/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ceeapp.apps.CeeappConfig',
+    'ceeapp',
+    'api',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -81,6 +83,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'api.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
