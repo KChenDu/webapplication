eadaaf2d751333cbe3b6596daf956d81a219026e
Kelvin <kelvenchendu@gmail.com>
2019-06-27 01:04:26 -0300

    New models added.

--- backend/ceeapp/models.py
+++ backend/ceeapp/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class Area(models.Model):
+    title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+
+class Company(models.Model):
+    area = models.ForeignKey(Area, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    website_url = models.CharField(max_length=200)
+    likes = models.PositiveIntegerField(default=0)
+    #image = models.ImageField(upload_to='uploads/')
+
+    def __str__(self):
+        return self.name
+
+    #def next_event_date(self):
+    #    return self.next_event >= timezone.now()
+
+    #datetime.timedelta(days=1)
+
+
+class Event(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    description = models.CharField(max_length=200)
+    date = models.DateTimeField('date')
+    #participants = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.description
